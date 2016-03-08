bf82da0c0a009db3cf375b53f5b4ea5c930ba494
Diogo Biazus <diogob@gmail.com>
2016-03-07 23:51:24 -0500

    Streams [Repo] instead of [Value]

--- src/HaskellTools/Github.hs
+++ src/HaskellTools/Github.hs
@@ -1,33 +1,40 @@
 module HaskellTools.Github
-    ( searchRepos
+    ( Repo (..)
     , haskellRepos
     ) where
 
 import Data.Monoid ((<>))
 
+import Data.List.Split (chunksOf)
+
+import Data.Maybe (mapMaybe)
+
 import Network.Wreq ( getWith
                     , Options
                     , Response
-                    , asJSON
-                    , header
                     , defaults
                     )
 import Network.Wreq.Lens (responseBody)
-import Control.Lens ( (&)
-                    , (.~)
-                    , (^..)
-                    )
+import Control.Lens ((^..))
 
-import Data.Aeson.Types (FromJSON
-                        , Object
-                        , Value
-                        )
+import Data.Aeson.Types (Value ( String, Number ))
 
 import qualified Data.ByteString.Lazy as BL
-import Data.Aeson.Lens (key, _String, values)
+import Data.Aeson.Lens (key, values)
 
 import Control.Monad (unless)
 import Pipes
+
+import Data.Text (Text)
+import Data.Scientific (Scientific)
+
+data Repo = Repo
+  { name :: Text
+  , owner :: Text
+  , url :: Text
+  , watchers :: Scientific
+  , forks :: Scientific
+  } deriving (Show, Eq)
 
 baseUrl :: String
 baseUrl = "https://api.github.com/"
@@ -40,18 +47,27 @@ searchRepos :: Options -> String -> IO (Response BL.ByteString)
 searchRepos options query =
   getJSON options $ "search/repositories?q=" ++ query
 
-haskellRepos :: Int -> Producer [Value] IO ()
+haskellRepos :: Int -> Producer [Repo] IO ()
 haskellRepos page = do
   repos <- lift $ haskellReposPage page
   unless (null repos) $ do
     yield repos
     haskellRepos $ page + 1
 
-haskellReposPage :: Int -> IO [Value]
+haskellReposPage :: Int -> IO [Repo]
 haskellReposPage page = do
-  results <- searchRepos defaults url
-  return $ results ^.. repos . (key "name" <> key "watchers" <> key "forks_count" <> owner)
+  results <- searchRepos defaults searchUrl
+  return $ resultsToRepos $ results ^.. repos . (key "name" <> _owner <> key "html_url" <> key "watchers_count" <> key "forks_count")
   where
-    owner = key "owner" . key "login"
+    _owner = key "owner" . key "login"
     repos = responseBody . key "items" . values
-    url = "a in%3Aname language%3Ahaskell created%3A>2013-10-01&per_page=100&page=" <> show page
+    searchUrl = "a in%3Aname language%3Ahaskell created%3A>2013-10-01&per_page=100&page=" <> show page
+    resultsToRepos = mapMaybe listToRepo . chunksOf 5
+    listToRepo [String n, String o, String u, Number w, Number f] = Just Repo
+      { name = n
+      , owner = o
+      , url = u
+      , watchers = w
+      , forks = f
+      }
+    listToRepo _ = Nothing
