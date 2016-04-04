564253b7f6e179ae1b974f287c145bd336060c4b
Gustavo Guichard <gustavoguichard@gmail.com>
2016-04-04 10:56:05 -0300

    Rename Model in Repo.View

--- frontend/src/HaskellTools/Repo/View.elm
+++ frontend/src/HaskellTools/Repo/View.elm
@@ -1,13 +1,13 @@
 module HaskellTools.Repo.View (..) where
 
 import HaskellTools.Action exposing (..)
-import HaskellTools.Repo.Model as Repo
+import HaskellTools.Repo.Model exposing (Model)
 import Html exposing (Html, li, text, a)
 import Html.Attributes exposing (href, target)
 import Signal
 
 
-view : Signal.Address Action -> Repo.Model -> Html
+view : Signal.Address Action -> Model -> Html
 view address model =
   li
     []
