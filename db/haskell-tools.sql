a2dc5dd77a80be842b94d4b10b055723e063d309
Diogo Biazus <diogob@gmail.com>
2016-04-16 14:14:48 -0400

    Adds database view to extract owner and repo name from packages

--- db/haskell-tools.sql
+++ db/haskell-tools.sql
@@ -25,6 +25,17 @@ CREATE TABLE public.packages (
   PRIMARY KEY (name)
 );
 
+CREATE VIEW public.package_repos AS
+SELECT
+  p.name,
+  r[1] as owner,
+  r[2] as repo
+FROM
+  packages p,
+  regexp_matches(repo_location, 'github.com[:\/]([^\/]*)\/([^\. ]*)') r
+WHERE
+  repo_location ~* 'github';
+
 -- API exposed through PostgREST
 CREATE SCHEMA api;
 
