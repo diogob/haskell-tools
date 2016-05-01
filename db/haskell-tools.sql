c89aa85888cc2d949b21c832bf238bd564d95b7a
Diogo Biazus <diogob@gmail.com>
2016-05-01 15:39:52 -0400

    Adds created_at/updated_at to tables

--- db/haskell-tools.sql
+++ db/haskell-tools.sql
@@ -5,7 +5,7 @@ CREATE DATABASE haskell_tools;
 -- Private data structures
 
 CREATE TABLE public.packages (
-  package_name text,
+  package_name text PRIMARY KEY,
   version text NOT NULL,
   license text NOT NULL,
   description text NOT NULL,
@@ -14,28 +14,66 @@ CREATE TABLE public.packages (
   package_url text NOT NULL,
   repo_type text,
   repo_location text,
-  PRIMARY KEY (package_name)
+  created_at timestamp NOT NULL default current_timestamp,
+  updated_at timestamp NOT NULL default current_timestamp
 );
 
 CREATE TABLE public.repos (
   package_name text PRIMARY KEY REFERENCES packages,
   stars integer NOT NULL DEFAULT 0,
   forks integer NOT NULL DEFAULT 0,
-  collaborators integer NOT NULL DEFAULT 1
+  collaborators integer NOT NULL DEFAULT 1,
+  created_at timestamp NOT NULL default current_timestamp,
+  updated_at timestamp NOT NULL default current_timestamp
 );
 
 CREATE TABLE public.dependencies (
   dependent text REFERENCES public.packages (package_name),
   dependency text REFERENCES public.packages (package_name),
   version_range text,
+  created_at timestamp NOT NULL default current_timestamp,
+  updated_at timestamp NOT NULL default current_timestamp,
   PRIMARY KEY (dependent, dependency)
 );
 
 CREATE TABLE public.extensions (
   package_name text REFERENCES public.packages (package_name),
   extension text,
+  created_at timestamp NOT NULL default current_timestamp,
+  updated_at timestamp NOT NULL default current_timestamp,
   PRIMARY KEY (package_name, extension)
 );
+
+-- Triggers to update updated_at columns
+CREATE FUNCTION public.update_updated_at()
+RETURNS TRIGGER
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+  NEW.updated_at := current_timestamp;
+  RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER update_updated_at
+BEFORE UPDATE ON public.packages
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at();
+
+CREATE TRIGGER update_updated_at
+BEFORE UPDATE ON public.dependencies
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at();
+
+CREATE TRIGGER update_updated_at
+BEFORE UPDATE ON public.extensions
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at();
+
+CREATE TRIGGER update_updated_at
+BEFORE UPDATE ON public.repos
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at();
 
 CREATE VIEW public.package_repos AS
 SELECT
