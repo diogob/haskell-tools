114d09f16dbf64f4af287a17952968221e51e4ba
Diogo Biazus <diogob@gmail.com>
2016-05-06 18:59:27 -0400

    Style changes in package list

--- frontend/src/HaskellTools/Package/View.elm
+++ frontend/src/HaskellTools/Package/View.elm
@@ -2,36 +2,43 @@ module HaskellTools.Package.View (..) where
 
 import HaskellTools.Action exposing (..)
 import HaskellTools.Package.Model exposing (Model)
-import Html exposing (Html, ul, li, text, a, div, h2)
+import Html exposing (Html, ul, li, text, a, div, h4, p)
 import Html.Attributes exposing (href, target)
 import Signal
 
+separator = text " - "
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   li
     []
-    [ div
+    [ p
       []
-      [ h2 [] [ text model.package_name ]
+      [ h4 [] [ text model.package_name ]
       , ul
         []
-        [ li [] [text ("Category: " ++ model.category)]
+        [ li [] [text model.description]
+        , li [] [text ("Category: " ++ model.category)]
         , li []
           [ a
             [ href ("http://hackage.haskell.org/package/" ++ model.package_name), target "blank" ]
             [ text "Hackage" ]
-          ]
-        , li []
-          [ a
+          , separator
+          , a
             [ href model.repo_location, target "blank" ]
             [ text "Source code" ]
           ]
-        , li [] [text ("Dependencies: " ++ (toString (List.length model.dependencies))) ]
-        , li [] [text ("Dependents: " ++ (toString (List.length model.dependents))) ]
-        , li [] [text ("Stars: " ++ (toString model.stars))]
-        , li [] [text ("Forks: " ++ (toString model.forks))]
-        , li [] [text ("Collaborators: " ++ (toString model.collaborators))]
+        , li []
+          [ text ((toString (List.length model.dependencies)) ++ " Dependencies")
+          , separator
+          , text ((toString (List.length model.dependents)) ++ " Dependents")
+          , separator
+          , text ((toString model.stars) ++ " Stars")
+          , separator
+          , text ((toString model.forks) ++ " Forks")
+          , separator
+          , text ((toString model.collaborators) ++ " Collaborators")
+          ]
         ]
       ]
     ]
