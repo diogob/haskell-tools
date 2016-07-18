dc34a237545bd800000e04bef3be64dfaad73b1a
Diogo Biazus <diogob@gmail.com>
2016-07-18 10:24:17 -0400

    Move all remaining Api related functions and types to Api module so it can be shared among other modules

--- frontend/src/HaskellTools/Api.elm
+++ frontend/src/HaskellTools/Api.elm
@@ -1,12 +1,20 @@
 module HaskellTools.Api exposing (apiUrl
                                  , Packages
                                  , Package
+                                 , ApiMsg (..)
+                                 , searchPackages
                                  )
 
-apiUrl : String -> String
-apiUrl = (++) "http://localhost:3000"
+import HttpBuilder exposing(..)
+import Task
+import Json.Encode as Encode
+import Json.Decode exposing (Decoder, succeed, string, list, int, at, (:=))
+import Json.Decode.Extra exposing (..)
 
 type alias Packages = List Package
+
+type ApiMsg
+  = FetchPackages (Result String Packages)
 
 type alias Package =
   { package_name  : String
@@ -27,3 +35,60 @@ type alias Package =
   , created_at    : String
   , updated_at    : String
 }
+
+apiUrl : String -> String
+apiUrl = (++) "http://localhost:3000"
+
+packagesUrl : String
+packagesUrl = apiUrl "/packages"
+
+searchUrl : String
+searchUrl = apiUrl "/rpc/package_search"
+
+getPackages : Cmd ApiMsg
+getPackages =
+  get packagesUrl
+    |> (send (jsonReader decodeModel) stringReader)
+    |> Task.perform toError toOk
+
+searchPackages : String -> Cmd ApiMsg
+searchPackages query =
+    let
+        body = Encode.object
+               [ ("query", Encode.string query) ]
+    in
+        post searchUrl
+            |> withHeaders [("Content-Type", "application/json"), ("Accept", "application/json")]
+            |> withHeader "Range" "0-99"
+            |> withJsonBody body
+            |> (send (jsonReader decodeModel) stringReader)
+            |> Task.perform toError toOk
+
+toError : a -> ApiMsg
+toError _ = FetchPackages (Err "Err")
+
+toOk : Response Packages -> ApiMsg
+toOk r = FetchPackages (Ok r.data)
+
+decodeModel : Decoder Packages
+decodeModel =
+  list (
+    succeed Package
+      |: ("package_name" := string)
+      |: ("version" := string)
+      |: ("license" := string)
+      |: ("description" := string)
+      |: ("category" := string)
+      |: ("homepage" := string)
+      |: ("package_url" := string)
+      |: ("repo_type" := string)
+      |: ("repo_location" := string)
+      |: ("stars" := int)
+      |: ("forks" := int)
+      |: ("collaborators" := int)
+      |: ("extensions" := (list string))
+      |: ("dependencies" := (list string))
+      |: ("dependents" := (list string))
+      |: ("created_at" := string)
+      |: ("updated_at" := string)
+  )
--- frontend/src/HaskellTools/PackageList.elm
+++ frontend/src/HaskellTools/PackageList.elm
@@ -2,19 +2,12 @@ module HaskellTools.PackageList exposing (Model, Msg(..), view, update, init)
 
 import Html exposing (Html, ul, li, text, a, div, h4, p)
 import Html.Attributes exposing (href, target)
-import Task
 import String
-
-import Json.Encode as Encode
-import Json.Decode exposing (Decoder, succeed, string, list, int, at, (:=))
-import Json.Decode.Extra exposing (..)
-
-import HttpBuilder exposing(..)
 
 import HaskellTools.Api exposing (..)
 
 type Msg
-  = FetchPackages (Result String Model)
+  = Api ApiMsg
   | SearchPackages String
 
 type alias Model = Packages
@@ -31,9 +24,9 @@ update action model =
   case action of
     SearchPackages query ->
         if String.length query > 0
-        then ( model, searchPackages query )
+        then ( model, Cmd.map Api (searchPackages query) )
         else ( [], Cmd.none )
-    FetchPackages result ->
+    Api (FetchPackages result) ->
       case result of
         Ok pkgs ->
           ( pkgs, Cmd.none )
@@ -44,59 +37,6 @@ init : (Model, Cmd Msg)
 init = ([], Cmd.none)
 
 -- private functions
-packagesUrl : String
-packagesUrl = apiUrl "/packages"
-
-searchUrl : String
-searchUrl = apiUrl "/rpc/package_search"
-
-getPackages : Cmd Msg
-getPackages =
-  get packagesUrl
-    |> (send (jsonReader decodeModel) stringReader)
-    |> Task.perform toError toOk
-
-searchPackages : String -> Cmd Msg
-searchPackages query =
-    let
-        body = Encode.object
-               [ ("query", Encode.string query) ]
-    in
-        post searchUrl
-            |> withHeaders [("Content-Type", "application/json"), ("Accept", "application/json")]
-            |> withHeader "Range" "0-99"
-            |> withJsonBody body
-            |> (send (jsonReader decodeModel) stringReader)
-            |> Task.perform toError toOk
-
-toError : a -> Msg
-toError _ = FetchPackages (Err "Err")
-
-toOk : Response Model -> Msg
-toOk r = FetchPackages (Ok r.data)
-
-decodeModel : Decoder Model
-decodeModel =
-  list (
-    succeed Package
-      |: ("package_name" := string)
-      |: ("version" := string)
-      |: ("license" := string)
-      |: ("description" := string)
-      |: ("category" := string)
-      |: ("homepage" := string)
-      |: ("package_url" := string)
-      |: ("repo_type" := string)
-      |: ("repo_location" := string)
-      |: ("stars" := int)
-      |: ("forks" := int)
-      |: ("collaborators" := int)
-      |: ("extensions" := (list string))
-      |: ("dependencies" := (list string))
-      |: ("dependents" := (list string))
-      |: ("created_at" := string)
-      |: ("updated_at" := string)
-  )
 
 separator : Html msg
 separator = text " - "
